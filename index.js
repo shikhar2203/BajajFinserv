712f92f0e6ee16e0701ace03baf3b9f1151938ef
shikhar2203 <shikharsharma2203@gmail.com>
2024-08-25 12:19:03 +0530

    changes done

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 const express = require("express");
-const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    userId: {
+        type: String,
+        required: true,
+    },
+    college: {
+        type: String,
+    },
+    collegeEmail: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    numbers:{
+        type: Array,
+    },
+    alphabets: {
+        type: Array,
+    },
+});
+
+app.get("/bfhl", async (req, res) => {
+
+    return res.json({operationCode: "1"});
+});
+
+app.post("/bfhl", async (req, res) => {
+    const body = req.body
+    return res.json(body);
+});
 

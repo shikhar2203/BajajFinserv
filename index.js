9df52a57b0078a5425dc6faa161c4d03da0414bb
shikhar2203 <shikharsharma2203@gmail.com>
2024-08-25 13:55:50 +0530

    fixed minor updates

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require("express");
 
+
+const app = express();
+const PORT = 8000;
+
 app.get("/bfhl", async (req, res) => {
 
     return res.json({operationCode: "1"});
 });
 
 app.post("/bfhl", async (req, res) => {
-    const body = req.body
+    const body = req.body;
     return res.json(body);
 });
 
+app.listen(PORT, () => console.log( `Server is running on port ${PORT}` ));
